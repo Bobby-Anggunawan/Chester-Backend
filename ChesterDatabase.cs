eabb670f80989ab56c1c6969b3a5c46c429319d0
Bobby-Anggunawan <yeremia.blog@gmail.com>
2022-06-26 20:19:41 +0700

    terhubung ke cloud sql

---
+++ ChesterDatabase.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+using MySql.Data.MySqlClient;
+
+namespace chesterBackendNet31
+{
+    public class ChesterDatabase
+    {
+        //===============================================
+        static string projectID = "chester-game";
+        static public FirestoreDb firestoreInstance = FirestoreDb.Create(projectID);
+        //===============================================
+
+        public class ChesterMySQL {
+
+            private MySqlConnection _connection;
+            MySqlConnection connection {
+                get {
+                    if(_connection == null) _connection = new MySqlConnection(getConnectionString());
+
+                    return _connection;
+                }
+            }
+
+            //========================================================================
+            private bool openConnection() {
+                try {
+                    connection.Open();
+                    return true;
+                }
+                catch {
+                    return false;
+                }
+            }
+            private void closeConnection() {
+                connection.Close();
+            }
+
+            //kalau return true artinya sukses
+            public bool addUser(string userID) {
+                string query = $"insert into user values('{userID}', 0)";
+
+                if (openConnection()) {
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+
+                    closeConnection();
+
+                    return true;
+                }
+                return false;
+            }
+
+            public List<Dictionary<string, object>> getUser() {
+                string query = "SELECT * FROM user";
+
+                List<Dictionary<string, object>> ret = new List<Dictionary<string, object>>();
+
+                if (openConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        ret.Add(new Dictionary<string, object> { { "userID", dataReader["userID"] }, { "coin", dataReader["coin"] } });
+                    }
+                    dataReader.Close();
+
+                    closeConnection();
+                }
+                else {
+                    ret.Add(new Dictionary<string, object> { { "isinya", "kosong" } });
+                }
+                return ret;
+            }
+            //========================================================================
+
+            static public string getConnectionString()
+            {
+                // Equivalent connection string:
+                // "Server=<dbSocketDir>/<INSTANCE_CONNECTION_NAME>;Uid=<DB_USER>;Pwd=<DB_PASS>;Database=<DB_NAME>;Protocol=unix"
+                String dbSocketDir = Environment.GetEnvironmentVariable("DB_SOCKET_PATH") ?? "/cloudsql";
+                String instanceConnectionName = Environment.GetEnvironmentVariable("INSTANCE_CONNECTION_NAME");
+                var connectionString = new MySqlConnectionStringBuilder()
+                {
+                    // The Cloud SQL proxy provides encryption between the proxy and instance.
+                    SslMode = MySqlSslMode.Disabled,
+                    // Remember - storing secrets in plain text is potentially unsafe. Consider using
+                    // something like https://cloud.google.com/secret-manager/docs/overview to help keep
+                    // secrets secret.
+                    Server = String.Format("{0}/{1}", dbSocketDir, instanceConnectionName),
+                    UserID = Environment.GetEnvironmentVariable("DB_USER"),   // e.g. 'my-db-user
+                    Password = Environment.GetEnvironmentVariable("DB_PASS"), // e.g. 'my-db-password'
+                    Database = Environment.GetEnvironmentVariable("DB_NAME"), // e.g. 'my-database'
+                    ConnectionProtocol = MySqlConnectionProtocol.UnixSocket
+                };
+                connectionString.Pooling = true;
+                // Specify additional properties here.
+                return connectionString.ConnectionString;
+            }
+        }
+    }
+}
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Google.Cloud.Firestore;
+using MySql.Data.MySqlClient;
 
 namespace chesterBackendNet31.Controllers
 {
@@ -42,11 +43,6 @@ namespace chesterBackendNet31.Controllers
     [Route("[controller]")]
     public class MatchmakingController : ControllerBase {
 
-        //===============================================
-        static string projectID = "chester-game";
-        FirestoreDb db = FirestoreDb.Create(projectID);
-        //===============================================
-
         private readonly ILogger<MatchmakingController> _logger;
 
         public MatchmakingController(ILogger<MatchmakingController> logger)
@@ -56,7 +52,7 @@ namespace chesterBackendNet31.Controllers
 
         [HttpGet("{userID}")]
         public JsonResult Get(string userID) {
-            CollectionReference usersRef = db.Collection("duel");
+            CollectionReference usersRef = ChesterDatabase.firestoreInstance.Collection("duel");
             QuerySnapshot snapshot = usersRef.GetSnapshotAsync().Result;
 
             string ret = "";
@@ -77,13 +73,13 @@ namespace chesterBackendNet31.Controllers
 
             if (selesaiCari)
             {
-                DocumentReference docRef = db.Collection("duel").Document(ret);
+                DocumentReference docRef = ChesterDatabase.firestoreInstance.Collection("duel").Document(ret);
                 docRef.UpdateAsync(new Dictionary<string, object> { { "redPlayerID", userID } });
                 isBlue = false;
             }
             else
             {
-                DocumentReference docRef = db.Collection("duel").Document();
+                DocumentReference docRef = ChesterDatabase.firestoreInstance.Collection("duel").Document();
                 ret = docRef.Id;
                 docRef.SetAsync(new Dictionary<string, object>{
                     { "bluePlayerID", userID}
@@ -93,4 +89,71 @@ namespace chesterBackendNet31.Controllers
             return new JsonResult(new Dictionary<string, object> { {"roomID", ret }, {"isBlue", isBlue} });
         }
     }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("{userID}")]
+        public String Post(string userID) {
+            var data = new ChesterDatabase.ChesterMySQL().addUser(userID);
+            if (data) return "Berhasil dirambah";
+            return "Gagal dittambah";
+        }
+
+        [HttpGet]
+        public JsonResult Get() {
+            var data = new ChesterDatabase.ChesterMySQL().getUser();
+            return new JsonResult(data);
+        }
+    }
+
+    [ApiController]
+    [Route("/")]
+    public class TestController : ControllerBase {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult Get() {
+            
+            string dbSocketDir = Environment.GetEnvironmentVariable("DB_SOCKET_PATH") ?? "/cloudsql";
+            string instanceConnectionName = Environment.GetEnvironmentVariable("INSTANCE_CONNECTION_NAME");
+            string UserID = Environment.GetEnvironmentVariable("DB_USER");   // e.g. 'my-db-user
+            string Password = Environment.GetEnvironmentVariable("DB_PASS");
+            string Database = Environment.GetEnvironmentVariable("DB_NAME");
+            return new JsonResult(
+                new Dictionary<string, string> { { "dbSocketDir", dbSocketDir }, { "instanceConnectionName", instanceConnectionName }, { "UserID", UserID }, { "Password", Password }, { "Database", Database }, {"Connection String", ChesterDatabase.ChesterMySQL.getConnectionString() } }
+            );
+            /*
+            string ret = "";
+            var connection = new MySqlConnection(ChesterDatabase.ChesterMySQL.getConnectionString());
+
+            try
+            {
+                connection.Open();
+                ret = "berhasil";
+            }
+            catch(Exception e)
+            {
+                ret = e.StackTrace;
+            }
+
+            ret += "\n\n"+Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            ret += "\n\n" + "Connection string = "+ ChesterDatabase.ChesterMySQL.getConnectionString();
+
+            return ret;*/
+        }
+    }
+
 }
